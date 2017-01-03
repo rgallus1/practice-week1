d647213f47e879ab342ac131d3566c3e4d503190
rgallus1 <roseannegallus@yahoo.com>
2017-01-03 13:08:52 -0800

    added pet table

--- app.js
+++ app.js
@@ -2,35 +2,26 @@
 //DOM variables for the table I want to produce.
 var petForm = document.getElementById('pet-form');//pet information
 var petName = document.getElementById('petName');//want to play?
-//var allPets = [];
+
 var initialNameEntered = false;
 var yesButton = document.getElementById('yesButton');//yes to play
 var noButton = document.getElementById('noButton');//no to play
 
-// var petData = function(petFirstName, petLastName, streetAddress, city, state, zip, dateOfBirth, chipped) {
-//   this.petFirstName = petFirstName;
-//   this.petLastName = petLastName;
-//   this.streetAddress = streetAddress;
-//   this.city = city;
-//   this.state = state;
-//   this.zip = zip;
-//   this.dateOfBirth = dateOfBirth;
-//   this.chipped = chipped;
-//   allPets.push(this); //this pushes the data above into the all Pets array
-// }
+var tablePets = document.getElementById('tablePets');
 
 
 //Check to see if the PetName is already in local storage
-if (localStorage.petName) {
-  petForm.innerHTML='';
-  var pEl = document.createElement('p');
-  pEl.textContent = JSON.parse(localStorage.getItem('petName')) + ', already exists, please enter a new name';
-  petName.appendChild(pEl);
-  //displayYesNoButtons();
-}
+// if (localStorage.petName) {
+//   petForm.innerHTML='';
+//   var pEl = document.createElement('p');
+//   pEl.textContent = JSON.parse(localStorage.getItem('petName')) + ', already exists, please enter a new name';
+//   petName.appendChild(pEl);
+//   //displayYesNoButtons();
+// }
 
 //Populate Pet Information
 function petHandler(event) {
+  console.log('pethandler function');
   event.preventDefault();
   localStorage.petName = JSON.stringify(event.target.petName.value);
   localStorage.streetAddress = JSON.stringify(event.target.streetAddress.value);
@@ -66,6 +57,51 @@ function displayYesNoButtons() {
   noButton.appendChild(newButtonNo);
 }
 
+// function yesAddAnother() {
+//   console.log('yes add another function')
+// }
+
+function makePetsChart(names, chipped) {
+  console.log(names);
+  var tr;
+  var td1;
+  var td2;
+  tr = document.createElement('tr');
+  td1 = document.createElement('td');
+  td1.textContent = 'PET NAME';
+  td2 = document.createElement('td');
+  td2.textContent = 'CHIPPED';
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tablePets.appendChild(tr);
+  for (var i = 0; i < chipped.length; i++) {
+    tr = document.createElement('tr');
+    td1 = document.createElement('td');
+    td1.textContent = names;
+    console.log(names);
+    td2 = document.createElement('td');
+    td2.textContent = chipped;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tablePets.appendChild(tr);
+  }
+}
+
+function makeTableHeading() {
+  var text = document.createElement('p');
+  text.textContent = 'Patient Information';
+}
+makeTableHeading();
+
+function renderPetTable() {
+  var localNames = JSON.parse(localStorage.getItem('petName'));
+  var localChipped = JSON.parse(localStorage.getItem('chipped'));
+  console.log('in render Pet Table', localNames);
+  makePetsChart(localNames, localChipped);
+}
+renderPetTable();
+
+
 petForm.addEventListener('submit', petHandler);
-//yesButton.addEventListener('click',yesLetsPlay);
-//noButton.addEventListener('click',noLetsNotPlay);
+// yesButton.addEventListener('click',yesAddAnother);
+///noButton.addEventListener('click', noDontAddAnother);
